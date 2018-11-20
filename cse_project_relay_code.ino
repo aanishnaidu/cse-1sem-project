773213444ee3f8724f41d89951da6c36dd9fdef3
aanishnaidu <42676767+aanishnaidu@users.noreply.github.com>
2018-11-20 14:53:59 +0530

    Add files via upload
    the program is in c++ language. the program is used to read input from the serial monitor and switch or off the relay. the code could be used for up to 4 relays.

---
+++ cse_project_relay_code.ino
@@ -0,0 +1,40 @@
+int x;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  x=Serial.read();
+  if (x=='1'){
+    digitalWrite(3,HIGH);
+    
+  }
+  else if (x=='2'){
+    digitalWrite(3,LOW);
+  }
+  else if (x=='3'){
+    digitalWrite(4,HIGH); 
+  }
+ else if (x=='4'){
+  digitalWrite(4,LOW);
+ }
+ else if (x=='5'){
+  digitalWrite(5,HIGH);
+ }
+ else if (x=='6'){
+  digitalWrite(5,LOW);
+ }
+ else if (x=='7'){
+  digitalWrite(6,HIGH);
+ }
+ else if(x=='8'){
+  digitalWrite(6,LOW);
+ }  
+ delay(10);
+}
